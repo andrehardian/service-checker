fc0eeb4eaf9cfe467b744437e7b518fa57495d1f
andre faisal <andre@logan.co.id>
2021-04-27 19:03:59 +0700

    add fix cm

--- app/src/main/java/com/ransom/servicechecker/ServiceManager.java
+++ app/src/main/java/com/ransom/servicechecker/ServiceManager.java
@@ -1,14 +1,55 @@
 package com.ransom.servicechecker;
 
+import android.content.Context;
+
+import connection.rxconnection.connection.ConnectionListener;
 import connection.rxconnection.connection.ConnectionManager;
+import connection.rxconnection.connection.HttpRequest;
 
 /**
  * Created by AndreHF on 11/14/2017.
  */
 
-public class ServiceManager extends ConnectionManager {
+public class ServiceManager extends ConnectionManager implements ConnectionListener {
+    private ConManager conManager;
+
+    @Override
+    public ConnectionManager setContext(Context context) {
+        conManager = new ConManager(context, this);
+        return super.setContext(context);
+    }
+
+    @Override
+    public ConnectionManager setConnectionListener(ConnectionListener connectionListener) {
+        return super.setConnectionListener(conManager);
+    }
+
     public void check() {
         subscribe(new ConAccess(getContext()).setLogInfoRequestResponse(true));
     }
 
+    @Override
+    public void onSuccessWithData(Object o) {
+
+    }
+
+    @Override
+    public void onSuccessNull() {
+
+    }
+
+    @Override
+    public void onMessageSuccess(String s) {
+
+    }
+
+    @Override
+    public void onError(Object o, HttpRequest httpRequest) {
+
+    }
+
+    @Override
+    public void unAuthorized(HttpRequest httpRequest, String messageError) {
+
+    }
 }
---
+++ sc/src/main/java/com/ransom/servicechecker/ConManager.java
@@ -0,0 +1,47 @@
+package com.ransom.servicechecker;
+
+import android.content.Context;
+
+import connection.rxconnection.connection.ConnectionListener;
+import connection.rxconnection.connection.HttpRequest;
+import connection.rxconnection.session.SessionLogin;
+
+public class ConManager implements ConnectionListener {
+    protected ConnectionListener listener;
+    private Context context;
+
+    public ConManager(Context context, ConnectionListener listener) {
+        this.context = context;
+        this.listener = listener;
+    }
+
+    @Override
+    public void onSuccessWithData(Object o) {
+        if (o instanceof ResponseAccess) {
+            new SessionLogin(context).clearToken();
+            listener.unAuthorized(null, null);
+            return;
+        }
+        listener.onSuccessWithData(o);
+    }
+
+    @Override
+    public void onSuccessNull() {
+        listener.onSuccessNull();
+    }
+
+    @Override
+    public void onMessageSuccess(String s) {
+        listener.onMessageSuccess(s);
+    }
+
+    @Override
+    public void onError(Object o, HttpRequest httpRequest) {
+        listener.onError(o, httpRequest);
+    }
+
+    @Override
+    public void unAuthorized(HttpRequest httpRequest, String messageError) {
+        listener.unAuthorized(httpRequest, messageError);
+    }
+}
