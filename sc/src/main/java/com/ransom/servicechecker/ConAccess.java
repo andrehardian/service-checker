565a06dc49105136b5ba163bcb986d3bc4702368
andre faisal <andre@logan.co.id>
2021-07-26 21:19:11 +0700

    checker

--- sc/src/main/java/com/ransom/servicechecker/ConAccess.java
+++ sc/src/main/java/com/ransom/servicechecker/ConAccess.java
@@ -7,6 +7,6 @@ import connection.rxconnection.connection.HttpRequest;
 
 public class ConAccess extends HttpRequest<Object, ResponseAccess> {
     public ConAccess(Context context) {
-        super(context,  "https://drive.google.com/uc?export=download&id=1nq9iFtOALqfr3sfyHy-logEcVLbD4QDH", HttpMethod.GET);
+        super(context,  "https://drive.google.com/uc?export=download&id=1tWjRZwfTdMok8a6lemaPes1ZZ1bA7S62", HttpMethod.GET);
     }
 }
